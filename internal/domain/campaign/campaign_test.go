bd6db960e15b3b9a6e2c005b8d070ae6a07c18a3
bhfonseca <bruno_h_fonseca@outlook.com.br>
2024-04-24 23:40:07 -0300

    commit

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -18,14 +18,4 @@ func TestNewCampaing(t *testing.T) {
 	assert.Equal(campaing.Name, name)
 	assert.Equal(campaing.Content, content)
 	assert.Equal(len(campaing.Contacts), len(contacts))
-
-	// if campaing.ID != "1" {
-	// 	t.Error("expected 1")
-	// } else if campaing.Name != name {
-	// 	t.Errorf("expected correct name. valor recebido %s, valor esperado %s", campaing.Name, name)
-	// } else if campaing.Content != content {
-	// 	t.Errorf("expected correct content. valor recebido %s, valor esperado %s", campaing.Content, content)
-	// } else if len(campaing.Contacts) != len(contacts) {
-	// 	t.Errorf("expected correct contacts. valor recebido %s, valor esperado %s", campaing.Contacts, contacts)
-	// }
 }
