a4a1a97e24ef191e8eb0aeb31c71cf3050de7f13
bhfonseca <bruno_h_fonseca@outlook.com.br>
2024-04-24 23:04:09 -0300

    commit

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -1,21 +1,31 @@
 package campaign
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewCampaing(t *testing.T) {
+	assert := assert.New(t)
 	name := "Campanha X"
 	content := "Body"
 	contacts := []string{"bruno@email.com", "brunao@ixcmail.com"}
 
 	campaing := NewCampaign(name, content, contacts)
 
-	if campaing.ID != "1" {
-		t.Error("expected 1")
-	} else if campaing.Name != name {
-		t.Errorf("expected correct name. valor recebido %s, valor esperado %s", campaing.Name, name)
-	} else if campaing.Content != content {
-		t.Errorf("expected correct content. valor recebido %s, valor esperado %s", campaing.Content, content)
-	} else if len(campaing.Contacts) != len(contacts) {
-		t.Errorf("expected correct contacts. valor recebido %s, valor esperado %s", campaing.Contacts, contacts)
-	}
+	assert.Equal(campaing.ID, "1")
+	assert.Equal(campaing.Name, name)
+	assert.Equal(campaing.Content, content)
+	assert.Equal(len(campaing.Contacts), len(contacts))
+
+	// if campaing.ID != "1" {
+	// 	t.Error("expected 1")
+	// } else if campaing.Name != name {
+	// 	t.Errorf("expected correct name. valor recebido %s, valor esperado %s", campaing.Name, name)
+	// } else if campaing.Content != content {
+	// 	t.Errorf("expected correct content. valor recebido %s, valor esperado %s", campaing.Content, content)
+	// } else if len(campaing.Contacts) != len(contacts) {
+	// 	t.Errorf("expected correct contacts. valor recebido %s, valor esperado %s", campaing.Contacts, contacts)
+	// }
 }
